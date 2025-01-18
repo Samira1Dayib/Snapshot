902f15c2f2c2ad337fed2b141f6c1b450e170beb
samiradayib <samiradayib1@gmail.com>
2025-01-18 17:16:23 -0500

    created repos

---
+++ src/main/java/com/example/snapshot/repository/CapsuleRepository.java
@@ -0,0 +1,11 @@
+package com.example.snapshot.repository;
+
+
+
+import com.example.snapshot.model.Capsule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CapsuleRepository extends JpaRepository<Capsule, Long> {
+
+}
+
